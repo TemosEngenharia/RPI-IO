6167dba170d6bb1e2b67ad7d56158610fb9575fa
cezarsantanna <cezar.santanna@gmail.com>
2016-07-04 09:30:59 -0300

    FIX issue #7

--- RPi_IO/models.py
+++ RPi_IO/models.py
@@ -14,9 +14,9 @@ from migrate.versioning import api
 from sqlalchemy.ext.declarative import declarative_base
 
 APP_DIR = path.dirname(path.realpath(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/../var/sqlite/RPi_IO.db' % APP_DIR
-SQLALCHEMY_MIGRATE_REPO = '%s/../var/sqlite/db_repository' % APP_DIR
-SQL_DATABASE_PATH = '%s/../var/sqlite/RPi_IO.db' % APP_DIR
+SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/../../var/sqlite/RPi_IO.db' % APP_DIR
+SQLALCHEMY_MIGRATE_REPO = '%s/../../var/sqlite/db_repository' % APP_DIR
+SQL_DATABASE_PATH = '%s/../../var/sqlite/RPi_IO.db' % APP_DIR
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 Base = declarative_base()
@@ -120,34 +120,32 @@ def create_db():
         api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
     else:
         api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
-
-    M1A = Module(name=u'Módulo 1A', slot=1, gpio=5, io_type='input', rpull=False, status=False, device_id='')
-    M1B = Module(name=u'Módulo 1B', slot=1, gpio=6, io_type='input', rpull=False, status=False, device_id='')
-    M1C = Module(name=u'Módulo 1C', slot=1, gpio=7, io_type='input', rpull=False, status=False, device_id='')
-    M2A = Module(name=u'Módulo 2A', slot=2, gpio=8, io_type='input', rpull=False, status=False, device_id='')
-    M2B = Module(name=u'Módulo 2B', slot=2, gpio=9, io_type='input', rpull=False, status=False, device_id='')
-    M2C = Module(name=u'Módulo 2C', slot=2, gpio=10, io_type='input', rpull=False, status=False, device_id='')
-    M3A = Module(name=u'Módulo 3A', slot=3, gpio=11, io_type='input', rpull=False, status=False, device_id='')
-    M3B = Module(name=u'Módulo 3B', slot=3, gpio=12, io_type='input', rpull=False, status=False, device_id='')
-    M3C = Module(name=u'Módulo 3C', slot=3, gpio=13, io_type='input', rpull=False, status=False, device_id='')
-    M4A = Module(name=u'Módulo 4A', slot=4, gpio=0, io_type='input', rpull=False, status=False, device_id='')
-    M4B = Module(name=u'Módulo 4B', slot=4, gpio=1, io_type='input', rpull=False, status=False, device_id='')
-    M4C = Module(name=u'Módulo 4C', slot=4, gpio=16, io_type='input', rpull=False, status=False, device_id='')
-    M5A = Module(name=u'Módulo 5A', slot=5, gpio=17, io_type='input', rpull=False, status=False, device_id='')
-    M5B = Module(name=u'Módulo 5B', slot=5, gpio=18, io_type='input', rpull=False, status=False, device_id='')
-    M5C = Module(name=u'Módulo 5C', slot=5, gpio=19, io_type='input', rpull=False, status=False, device_id='')
-    M6A = Module(name=u'Módulo 6A', slot=6, gpio=20, io_type='input', rpull=False, status=False, device_id='')
-    M6B = Module(name=u'Módulo 6B', slot=6, gpio=21, io_type='input', rpull=False, status=False, device_id='')
-    M6C = Module(name=u'Módulo 6C', slot=6, gpio=22, io_type='input', rpull=False, status=False, device_id='')
-    M7A = Module(name=u'Módulo 7A', slot=7, gpio=23, io_type='input', rpull=False, status=False, device_id='')
-    M7B = Module(name=u'Módulo 7B', slot=7, gpio=24, io_type='input', rpull=False, status=False, device_id='')
-    M7C = Module(name=u'Módulo 7C', slot=7, gpio=25, io_type='input', rpull=False, status=False, device_id='')
+    M1A = Module(name=u'M1A', slot=1, gpio=8, io_type='input', rpull=False, status=False, device_id='')
+    M1B = Module(name=u'M1B', slot=1, gpio=7, io_type='input', rpull=False, status=False, device_id='')
+    M1C = Module(name=u'M1C', slot=1, gpio=11, io_type='input', rpull=False, status=False, device_id='')
+    M2A = Module(name=u'M2A', slot=2, gpio=9, io_type='input', rpull=False, status=False, device_id='')
+    M2B = Module(name=u'M2B', slot=2, gpio=10, io_type='input', rpull=False, status=False, device_id='')
+    M2C = Module(name=u'M2C', slot=2, gpio=5, io_type='input', rpull=False, status=False, device_id='')
+    M3A = Module(name=u'M3A', slot=3, gpio=6, io_type='input', rpull=False, status=False, device_id='')
+    M3B = Module(name=u'M3B', slot=3, gpio=12, io_type='input', rpull=False, status=False, device_id='')
+    M3C = Module(name=u'M3C', slot=3, gpio=13, io_type='input', rpull=False, status=False, device_id='')
+    M4A = Module(name=u'M4A', slot=4, gpio=0, io_type='input', rpull=False, status=False, device_id='')
+    M4B = Module(name=u'M4B', slot=4, gpio=1, io_type='input', rpull=False, status=False, device_id='')
+    M4C = Module(name=u'M4C', slot=4, gpio=16, io_type='input', rpull=False, status=False, device_id='')
+    M5A = Module(name=u'M5A', slot=5, gpio=17, io_type='input', rpull=False, status=False, device_id='')
+    M5B = Module(name=u'M5B', slot=5, gpio=18, io_type='input', rpull=False, status=False, device_id='')
+    M5C = Module(name=u'M5C', slot=5, gpio=19, io_type='input', rpull=False, status=False, device_id='')
+    M6A = Module(name=u'M6A', slot=6, gpio=20, io_type='input', rpull=False, status=False, device_id='')
+    M6B = Module(name=u'M6B', slot=6, gpio=21, io_type='input', rpull=False, status=False, device_id='')
+    M6C = Module(name=u'M6C', slot=6, gpio=22, io_type='input', rpull=False, status=False, device_id='')
+    M7A = Module(name=u'M7A', slot=7, gpio=23, io_type='input', rpull=False, status=False, device_id='')
+    M7B = Module(name=u'M7B', slot=7, gpio=24, io_type='input', rpull=False, status=False, device_id='')
+    M7C = Module(name=u'M7C', slot=7, gpio=25, io_type='input', rpull=False, status=False, device_id='')
     modules = [M1A, M1B, M1C, M2A, M2B, M2C, M3A, M3B, M3C, M4A, M4B, M4C,
                M5A, M5B, M5C, M6A, M6B, M6C, M7A, M7B, M7C]
     for m in modules:
         session.add(m)
     session.commit()
-
 
 def migrate_db():
     import imp
@@ -169,6 +167,39 @@ def downgrade_db():
     v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
     api.downgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, v - 1)
     v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+
+def reset_db():
+    # engine = create_engine(SQLALCHEMY_DATABASE_URI)
+    # Base = declarative_base()
+    # Session = sessionmaker(bind=engine)
+    # session = Session()
+
+    for name, gpio in session.query(Module.name, Module.gpio):
+        print(name, gpio)
+    session.query(Module).filter(Module.name == 'M1A').update({'gpio': 8})
+    session.query(Module).filter(Module.name == 'M1B').update({"gpio": 7})
+    session.query(Module).filter(Module.name == 'M1C').update({"gpio": 11})
+    session.query(Module).filter(Module.name == 'M2A').update({"gpio": 9})
+    session.query(Module).filter(Module.name == 'M2B').update({"gpio": 10})
+    session.query(Module).filter(Module.name == 'M2C').update({"gpio": 5})
+    session.query(Module).filter(Module.name == 'M3A').update({"gpio": 6})
+    session.query(Module).filter(Module.name == 'M3B').update({"gpio": 12})
+    session.query(Module).filter(Module.name == 'M3C').update({"gpio": 13})
+    session.query(Module).filter(Module.name == 'M4A').update({"gpio": 0})
+    session.query(Module).filter(Module.name == 'M4B').update({"gpio": 1})
+    session.query(Module).filter(Module.name == 'M4C').update({"gpio": 16})
+    session.query(Module).filter(Module.name == 'M5A').update({"gpio": 17})
+    session.query(Module).filter(Module.name == 'M5B').update({"gpio": 18})
+    session.query(Module).filter(Module.name == 'M5C').update({"gpio": 19})
+    session.query(Module).filter(Module.name == 'M6A').update({"gpio": 20})
+    session.query(Module).filter(Module.name == 'M6B').update({"gpio": 21})
+    session.query(Module).filter(Module.name == 'M6C').update({"gpio": 22})
+    session.query(Module).filter(Module.name == 'M7A').update({"gpio": 23})
+    session.query(Module).filter(Module.name == 'M7B').update({"gpio": 24})
+    session.query(Module).filter(Module.name == 'M7C').update({"gpio": 25})
+    session.commit()
+    for name, gpio in session.query(Module.name, Module.gpio):
+        print(name, gpio)
 
 if not path.exists(SQL_DATABASE_PATH):
     print 'criando DataBase'
